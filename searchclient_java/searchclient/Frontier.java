ea6a1a4eb4429b5d3dc24350babdbbfadda62acb
Jd <alofa.jeandavid@gmail.com>
2024-05-14 14:08:24 +0200

    Init

---
+++ searchclient_java/searchclient/Frontier.java
@@ -0,0 +1,141 @@
+package searchclient;
+
+import java.util.ArrayDeque;
+import java.util.HashSet;
+import java.util.PriorityQueue;
+
+public interface Frontier {
+    void add(State state);
+
+    State pop();
+
+    boolean isEmpty();
+
+    int size();
+
+    boolean contains(State state);
+
+    String getName();
+}
+
+class FrontierBFS
+        implements Frontier {
+    private final ArrayDeque<State> queue = new ArrayDeque<>(65536);
+    private final HashSet<State> set = new HashSet<>(65536);
+
+    @Override
+    public void add(State state) {
+        this.queue.addLast(state);
+        this.set.add(state);
+    }
+
+    @Override
+    public State pop() {
+        State state = this.queue.pollFirst();
+        this.set.remove(state);
+        return state;
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return this.queue.isEmpty();
+    }
+
+    @Override
+    public int size() {
+        return this.queue.size();
+    }
+
+    @Override
+    public boolean contains(State state) {
+        return this.set.contains(state);
+    }
+
+    @Override
+    public String getName() {
+        return "breadth-first search";
+    }
+}
+
+class FrontierDFS implements Frontier {
+    private final ArrayDeque<State> queue = new ArrayDeque<>(65536);
+    private final HashSet<State> set = new HashSet<>(65536);
+
+    @Override
+    public void add(State state) {
+        this.queue.addLast(state);
+        this.set.add(state);
+    }
+
+    @Override
+    public State pop() {
+        State state = this.queue.pollLast();
+        this.set.remove(state);
+        return state;
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return this.queue.isEmpty();
+    }
+
+    @Override
+    public int size() {
+        return this.queue.size();
+    }
+
+    @Override
+    public boolean contains(State state) {
+        return this.set.contains(state);
+    }
+
+    @Override
+    public String getName() {
+        return "depth-first search";
+    }
+}
+
+class FrontierBestFirst
+        implements Frontier {
+    private PriorityQueue<State> queue;
+    private HashSet<State> set = new HashSet<>();
+    private Heuristic heuristic;
+
+    public FrontierBestFirst(Heuristic h) {
+        this.heuristic = h;
+        this.queue = new PriorityQueue<>(this.heuristic);
+    }
+
+    @Override
+    public void add(State state) {
+        this.queue.add(state);
+        this.set.add(state);
+    }
+
+    @Override
+    public State pop() {
+        State state = this.queue.poll();
+        this.set.remove(state);
+        return state;
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return this.queue.isEmpty();
+    }
+
+    @Override
+    public int size() {
+        return this.queue.size();
+    }
+
+    @Override
+    public boolean contains(State state) {
+        return this.set.contains(state);
+    }
+
+    @Override
+    public String getName() {
+        return String.format("best-first search using %s", this.heuristic.toString());
+    }
+}
---
+++ searchclient_java/searchclient/Heuristic.java
@@ -0,0 +1,116 @@
+package searchclient;
+
+import java.util.Comparator;
+
+public abstract class Heuristic
+        implements Comparator<State> {
+            
+            private int[][][][] distances;
+
+            public Heuristic(State initialState) {
+                // Here's a chance to pre-process the static parts of the level.
+                this.distances = initialState.getDistances();
+        
+            }
+    public int h(State s) {
+        int count = 0;
+        for (int row = 1; row < s.goals.length - 1; row++) {
+            for (int col = 1; col < s.goals[row].length - 1; col++) {
+                char goal = s.goals[row][col];
+
+                if ('A' <= goal && goal <= 'Z' && s.boxes[row][col] != goal) {
+                    count++;
+                } else if ('0' <= goal && goal <= '9' &&
+                        !(s.agentRows[goal - '0'] == row && s.agentCols[goal - '0'] == col)) {
+                    count++;
+                }
+            }
+        }
+        return count;
+    }
+
+    public int H(State s) { /* improved heuristic */
+        int totalDistance = 0;
+        int numAgents = s.agentRows.length;
+        // For each agent...
+        for (int i = 0; i < numAgents; i++) {
+            // Find the distance to the nearest goal
+            int minDistance = Integer.MAX_VALUE;
+            for (int row = 1; row < s.goals.length - 1; row++) {
+                for (int col = 1; col < s.goals[row].length - 1; col++) {
+                    char goal = s.goals[row][col];
+                    if ('0' <= goal && goal <= '9') {
+                        int distance = this.distances[s.agentRows[i]][s.agentCols[i]][row][col];
+                        minDistance = Math.min(minDistance, distance);
+                    }
+                }
+            }
+            totalDistance += minDistance;
+        }
+        return totalDistance;
+
+    }
+
+
+
+    public abstract int f(State s);
+
+    @Override
+    public int compare(State s1, State s2) {
+        return this.f(s1) - this.f(s2);
+    }
+}
+
+class HeuristicAStar
+        extends Heuristic {
+    public HeuristicAStar(State initialState) {
+        super(initialState);
+    }
+
+    @Override
+    public int f(State s) {
+        return s.g() + this.H(s);
+    }
+
+    @Override
+    public String toString() {
+        return "A* evaluation";
+    }
+}
+
+class HeuristicWeightedAStar
+        extends Heuristic {
+    private int w;
+
+    public HeuristicWeightedAStar(State initialState, int w) {
+        super(initialState);
+        this.w = w;
+    }
+
+    @Override
+    public int f(State s) {
+        return s.g() + this.w * this.H(s);
+    }
+
+    @Override
+    public String toString() {
+        return String.format("WA*(%d) evaluation", this.w);
+    }
+}
+
+class HeuristicGreedy
+        extends Heuristic {
+    public HeuristicGreedy(State initialState) {
+        super(initialState);
+    }
+
+    @Override
+    public int f(State s) {
+        return this.H(s);
+    }
+
+    @Override
+    public String toString() {
+        return "greedy evaluation";
+    }
+}
